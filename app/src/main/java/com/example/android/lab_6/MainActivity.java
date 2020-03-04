cb7b8c1957bc47df222c18f2ef01b2d3660d34da
Montelto <mcfarland3@wisc.edu>
2020-03-04 17:44:25 -0600

    Done

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.android.lab_6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
     private final LatLng mDestinationLatLng = new LatLng(43.0752817,-89.4063554);
+    private LatLng mMyPosition;
     private GoogleMap mMap;
 
     @Override
@@ -23,6 +34,31 @@ public class MainActivity extends AppCompatActivity {
             mMap = googleMap;
             googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng)
                     .title("Destination"));
+            displayMyLocation();
         });
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    private void displayMyLocation() {
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            displayMyLocation();
+        } else {
+            mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task -> {
+               Location mLastKnownLocation = task.getResult();
+               if (task.isSuccessful() && mLastKnownLocation != null) {
+                   double latitude = mLastKnownLocation.getLatitude();
+                   double longitude = mLastKnownLocation.getLongitude();
+                   mMyPosition = new LatLng(latitude, longitude);
+                   mMap.addPolyline(new PolylineOptions().add(mMyPosition, mDestinationLatLng));
+                   mMap.addMarker(new MarkerOptions().position(mMyPosition)
+                           .title("My Position"));
+               }
+            });
+        }
     }
 }
